75e730892f568960ad897ea5c91ae1ed816605d9
devlopersabbir <devlopersabbir@gmail.com>
2023-09-22 18:23:29 +0600

    header finished with back button

--- source/app/Server.java
+++ source/app/Server.java
@@ -2,8 +2,11 @@ package source.app;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Server extends JFrame {
+public class Server extends JFrame implements ActionListener {
+    JPanel panel = new JPanel();
+    static Font font = new Font("SansSerif", Font.BOLD, 30);
 
     Server() {
         // window layout
@@ -17,11 +20,15 @@ public class Server extends JFrame {
         new Server();
     }
 
+    public void actionPerformed(ActionEvent event){
+
+    }
+
     public void runWindow() {
-        Font font = new Font("SansSerif", Font.BOLD, 30);
         setTitle("Chat Application");
         setSize(450, 700);
         setResizable(false);
+        setLocation(200, 50);
         getContentPane().setFont(font);
         getContentPane().setBackground(Color.white);
     }
@@ -29,9 +36,30 @@ public class Server extends JFrame {
     public void windowLayout() {
         setLayout(null);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(7, 94, 84));
+        panel.setBackground(new Color(7,94,84));
         panel.setBounds(0, 0, 450, 70);
         add(panel);
+        panel.setLayout(null);
+        leftIcon();
+    }
+
+    public void leftIcon() {
+        ImageIcon icon = new ImageIcon(getClass().getResource("/icons/left.png"));
+
+        Image img = icon.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon cnvIcon = new ImageIcon(img);
+
+
+        JLabel back = new JLabel(cnvIcon);
+        back.setLayout(null);
+        back.setBounds(5, 20, 25, 25);
+        panel.add(back);
+
+        back.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                System.exit(0);
+            }
+        });
     }
 }
